2913bd2cb646a869d006dd64e4f0379a60864965
Lingga Aradhana Sahadewa <linggaaradhanas@gmail.com>
2026-02-05 21:23:34 +0800

    Added features to split raw markdown into images and links

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -47,3 +47,68 @@ def extract_markdown_links(text):
     # \((.*?)\) -> capture URL
     pattern = r"(?<!!)\[(.*?)\]\((.*?)\)"
     return re.findall(pattern, text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+            
+        for image in images:
+            image_alt = image[0]
+            image_link = image[1]
+            sections = original_text.split(f"![{image_alt}]({image_link})", 1)
+            
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+            
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+            
+            new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+            original_text = sections[1]
+            
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+            
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+            
+        original_text = old_node.text
+        links = extract_markdown_links(original_text)
+        if len(links) == 0:
+            new_nodes.append(old_node)
+            continue
+            
+        for link in links:
+            link_text = link[0]
+            link_url = link[1]
+            sections = original_text.split(f"[{link_text}]({link_url})", 1)
+            
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, link section not closed")
+            
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+                
+            new_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+            original_text = sections[1]
+            
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TextType.TEXT))
+            
+    return new_nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,7 @@
 import unittest
 from textnode import TextNode, TextType
-from inline_markdown import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
+from inline_markdown import split_nodes_delimiter, extract_markdown_images,\
+    extract_markdown_links, split_nodes_image, split_nodes_link
 
 class TestInlineMarkdown(unittest.TestCase):
     def test_delim_bold(self):
@@ -55,6 +56,36 @@ class TestInlineMarkdown(unittest.TestCase):
                 ("to youtube", "https://www.youtube.com/@bootdotdev")
             ],
             matches
+        )
+    
+    def test_split_image(self):
+        node = TextNode(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            ],
+            new_nodes,
+        )
+
+    def test_split_links(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"),
+            ],
+            new_nodes,
         )
 
 if __name__ == "__main__":
